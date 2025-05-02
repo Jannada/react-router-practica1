7221c70c1e07973d01e6b647711fc071e22ea0b4
Mimi Winchester <vivianabeltre0912@gmail.com>
2025-05-02 14:07:59 -0400

    Actualización 2

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,15 +1,40 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
+import { useEffect } from "react";
+
 export const Blog = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  // useEffect(() => {
+  //   // setSearchParams({ filter: "Juan" });
+  // }, [searchParams]);
+  // // // Solo para fines ilustrativos
+
   const { data, loading, error } = useFetch(
     "https://jsonplaceholder.typicode.com/posts"
   );
 
   if (loading) return <p>Loading data...</p>;
   if (error) return <p>Error...</p>;
+
+  const handleChange = (e) => {
+    let filter = e.target.value;
+    if (filter) {
+      setSearchParams({ filter: filter });
+    } else {
+      setSearchParams({});
+    }
+  };
   return (
     <>
       <h1>Blog</h1>
+      <input
+        type="text"
+        name=""
+        onChange={handleChange}
+        className="form-control my-3"
+        value={searchParams.get("filter") || ""}
+      />
       <ul className="list-group">
         {data.map((item) => (
           <Link
